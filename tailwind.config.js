7f63f3bb69b13e94d76fa1333c2b1caa9acdca64
Pierre Santos <santospierre1305@gmail.com>
2020-12-24 13:12:49 +0100

    Install TailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ['./dist/**/*.html', './dist/**/*.js'],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
